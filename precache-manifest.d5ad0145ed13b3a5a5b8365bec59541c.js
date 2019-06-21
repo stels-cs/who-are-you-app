b94c959914fbf7fabf0505d5ef91218413288453
Иван Недзвецкий <stels-cs@yandex.ru>
2019-06-21 20:08:10 +0300

    Updates

---
+++ precache-manifest.d5ad0145ed13b3a5a5b8365bec59541c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4491c0b5208d15a5eaa558d45ad065a8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "491093225af75ecde0ce",
+    "url": "./static/css/2.2a4f5425.chunk.css"
+  },
+  {
+    "revision": "df8323e6208094809dab",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "491093225af75ecde0ce",
+    "url": "./static/js/2.89e3d013.chunk.js"
+  },
+  {
+    "revision": "df8323e6208094809dab",
+    "url": "./static/js/main.1df86f2c.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
